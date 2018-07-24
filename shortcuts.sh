17f34734b7c7aa8b00c73082e075cbd8b6e0a3b7
Luke Smith <luke@lukesmith.xyz>
2018-07-24 16:28:28 -0400

    rewrite, way better, faster and minimal

--- shortcuts.sh
+++ shortcuts.sh
@@ -5,45 +5,26 @@ folders="$HOME/.scripts/folders"
 configs="$HOME/.scripts/configs"
 
 # Output locations
-bash_shortcuts="$HOME/.shortcuts"
+shell_shortcuts="$HOME/.shortcuts"
 ranger_shortcuts="$HOME/.config/ranger/shortcuts.conf"
 qute_shortcuts="$HOME/.config/qutebrowser/shortcuts.py"
 
+# Shell rc file (i.e. bash vs. zsh, etc.)
+shellrc="$HOME/.bashrc"
+
+# Remove
+rm -f $shell_shortcuts $ranger_shortcuts $qute_shortcuts
+
 # Ensuring that output locations are properly sourced
-(cat $HOME/.bashrc | grep "source ~/.shortcuts")>/dev/null || echo "source ~/.shortcuts" >> $HOME/.bashrc
-(cat $HOME/.config/ranger/rc.conf | grep "source $HOME/.config/ranger/shortcuts.conf")>/dev/null || echo "source $HOME/.config/ranger/shortcuts.conf" >> $HOME/.config/ranger/rc.conf
-(cat $HOME/.config/qutebrowser/config.py | grep "config.source('shortcuts.py')")>/dev/null || echo "config.source('shortcuts.py')" >> $HOME/.config/qutebrowser/config.py
+(grep "source ~/.shortcuts"  $shellrc)>/dev/null || echo "source ~/.shortcuts" >> $shellrc
+(grep "source $HOME/.config/ranger/shortcuts.conf" $HOME/.config/ranger/rc.conf)>/dev/null || echo "source $HOME/.config/ranger/shortcuts.conf" >> $HOME/.config/ranger/rc.conf
+(grep "config.source('shortcuts.py')" $HOME/.config/qutebrowser/config.py)>/dev/null || echo "config.source('shortcuts.py')" >> $HOME/.config/qutebrowser/config.py
 
-#Delete old shortcuts
-echo "# vim: filetype=sh" > $bash_shortcuts
-echo "# ranger shortcuts" > $ranger_shortcuts
-echo "# qutebrowser shortcuts" > $qute_shortcuts
+# directory shortcuts
+cat $folders | sed "/^#/d" | awk '{print "alias "$1"=\"cd "$2" && ls -a\""}' >> $shell_shortcuts
+cat $folders | sed "/^#/d" | awk '{print "map g"$1" cd "$2"\nmap t"$1" tab_new "$2"\nmap m"$1" shell mv -v %s "$2"\nmap Y"$1" shell cp -rv %s "$2}' >> $ranger_shortcuts
+cat $folders | sed "/^#/d" | awk '{print "config.bind(\";"$1"\", \"set downloads.location.directory "$2" ;; hint links download\")"}' >> $qute_shortcuts
 
-writeDirs() { echo "alias $1='cd $2 && ls -a'" >> $bash_shortcuts
-	echo "map g$1 cd $2" >> $ranger_shortcuts
-	echo "map t$1 tab_new $2" >> $ranger_shortcuts
-	echo "map m$1 shell mv -v %s $2" >> $ranger_shortcuts
-	echo "map Y$1 shell cp -rv %s $2" >> $ranger_shortcuts
-	echo "config.bind(';$1', 'set downloads.location.directory $2 ;; hint links download')" >> $qute_shortcuts ;}
-
-writeConfs() {
-	echo "alias $1='vim $2'" >> $bash_shortcuts
-	echo "map $1 shell vim $2" >> $ranger_shortcuts ;}
-
-IFS=$'\n'
-set -f
-for line in $(cat "$folders"); do
-	line=$(echo $line | sed 's/#.*//')
-	key=$(echo $line | awk '{print $1}')
-	dir=$(echo $line | awk '{print $2}')
-	[ "$dir" == "" ] || writeDirs $key $dir
-done
-
-set -f
-for line in $(cat "$configs");
-do
-	line=$(echo $line | sed 's/#.*//')
-	short=$(echo $line | awk '{print $1}')
-	conf=$(echo $line | awk '{print $2}')
-	[ "$conf" == "" ] || writeConfs $short $conf
-done
+# dotfile shortcuts
+cat $configs | sed "/^#/d" | awk '{print "alias "$1"=\"$EDITOR "$2"\""}' >> $shell_shortcuts
+cat $configs | sed "/^#/d" | awk '{print "map "$1" shell $EDITOR "$2}' >> $ranger_shortcuts
