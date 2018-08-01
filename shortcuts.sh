72535c7521f866be02f12bd3557b0d38d812f91b
Luke Smith <luke@lukesmith.xyz>
2018-08-01 15:03:26 -0400

    minor efficiency

--- shortcuts.sh
+++ shortcuts.sh
@@ -25,10 +25,10 @@ rm -f $shell_shortcuts $ranger_shortcuts $qute_shortcuts
 (grep "config.source('shortcuts.py')" $HOME/.config/qutebrowser/config.py)>/dev/null || echo "config.source('shortcuts.py')" >> $HOME/.config/qutebrowser/config.py
 
 # directory shortcuts
-cat $folders | sed "/^#/d" | awk '{print "alias "$1"=\"cd "$2" && ls -a\""}' >> $shell_shortcuts
-cat $folders | sed "/^#/d" | awk '{print "map g"$1" cd "$2"\nmap t"$1" tab_new "$2"\nmap m"$1" shell mv -v %s "$2"\nmap Y"$1" shell cp -rv %s "$2}' >> $ranger_shortcuts
-cat $folders | sed "/^#/d" | awk '{print "config.bind(\";"$1"\", \"set downloads.location.directory "$2" ;; hint links download\")"}' >> $qute_shortcuts
+sed "/^#/d" $folders | awk '{print "alias "$1"=\"cd "$2" && ls -a\""}' >> $shell_shortcuts
+sed "/^#/d" $folders | awk '{print "map g"$1" cd "$2"\nmap t"$1" tab_new "$2"\nmap m"$1" shell mv -v %s "$2"\nmap Y"$1" shell cp -rv %s "$2}' >> $ranger_shortcuts
+sed "/^#/d" $folders | awk '{print "config.bind(\";"$1"\", \"set downloads.location.directory "$2" ;; hint links download\")"}' >> $qute_shortcuts
 
 # dotfile shortcuts
-cat $configs | sed "/^#/d" | awk '{print "alias "$1"=\"$EDITOR "$2"\""}' >> $shell_shortcuts
-cat $configs | sed "/^#/d" | awk '{print "map "$1" shell $EDITOR "$2}' >> $ranger_shortcuts
+sed "/^#/d" $configs | awk '{print "alias "$1"=\"$EDITOR "$2"\""}' >> $shell_shortcuts
+sed "/^#/d" $configs | awk '{print "map "$1" shell $EDITOR "$2}' >> $ranger_shortcuts
